05624e872abb6da1cb569723b26c8254af53fd9b
Shubham Rana <ranashubham7296@gmail.com>
2020-08-12 19:25:26 +0530

    Refactoring flip_UC1 to show number of times heads or tails come

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,4 +2,22 @@
 
 echo "Welcome to Flip coin simulator"
 
-echo "$((RANDOM%2))"
+read -p "Enter number of times you want to flip the coin: " count
+
+tailNum=0
+headNum=0
+
+for ((i=0; i<$count; i++))
+do
+	face="$((RANDOM%2))"
+
+	if [ $face -eq 1 ]
+	then
+		tailNum=$(($tailNum+1))
+	else
+		headNum=$(($headNum+1))
+	fi
+done
+
+echo "Tails showed up ${tailNum}"
+echo "Heads showed up ${headNum}"
